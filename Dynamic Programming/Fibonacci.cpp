9651d56c84a25b39a20b251505a59a68db3b0616
pro-ady <48754591+pro-ady@users.noreply.github.com>
2019-07-05 18:54:50 +0530

    Baby Steps into DP

--- Dynamic Programming/Fibonacci.cpp
+++ Dynamic Programming/Fibonacci.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
-int main(){
+int memo_pad[1000];
 
+int fib(int n){
+    if(n==0)
+        return 0;
+    if(n==1)
+        return 1;
+
+    if(memo_pad[n] != -1){
+        return memo_pad[n];
+    }
+    memo_pad[n] = fib(n-1) + fib(n-2);
+    return memo_pad[n];
+}
+
+int main(){
+    int n;
+    cin>>n;
+
+    // Compute the nth Fibonacci Number
+    for(int i=0;i<=n;i++){
+        memo_pad[i] = -1;
+    }
+
+    cout << fib(n);
 }
